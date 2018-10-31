cc2d6bf63fa57d51af8006be39912f4edc32e6ea
AjiSetya <setyaaji07@gmail.com>
2018-10-31 09:37:39 +0700

    add adapter todo

--- app/src/main/java/com/blogspot/blogsetyaaji/mytodolist/Fragment/TodoFragment.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/mytodolist/Fragment/TodoFragment.java
@@ -3,19 +3,31 @@ package com.blogspot.blogsetyaaji.mytodolist.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.blogspot.blogsetyaaji.mytodolist.R;
+import com.blogspot.blogsetyaaji.mytodolist.db.MyDatabaseHelper;
 import com.blogspot.blogsetyaaji.mytodolist.model.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TodoFragment extends Fragment {
 
+    private RecyclerView lvTodo;
+    private List<Todo> todoList = new ArrayList<>();
+    private LinearLayout todoKosong;
+    private TodoAdapter todoAdapter;
+
+    private MyDatabaseHelper myDatabaseHelper;
 
     public TodoFragment() {
         // Required empty public constructor
@@ -34,7 +46,19 @@ public class TodoFragment extends Fragment {
         //toodo.setNama("bermain bola");
         // tampilkan nama
         //Log.d("NAMA", todoo.getNama());
-        return inflater.inflate(R.layout.fragment_todo, container, false);
+        View view = inflater.inflate(R.layout.fragment_todo, container, false);
+
+        lvTodo = view.findViewById(R.id.lvTodo);
+        todoKosong = view.findViewById(R.id.todoKosong);
+
+        myDatabaseHelper = new MyDatabaseHelper(getActivity());
+
+        // meletakkan semua data ke dalam list
+        todoList.addAll(myDatabaseHelper.ambilSemuaData());
+
+        todoAdapter = new TodoAdapter(getActivity(), todoList);
+
+        return view;
     }
 
 }
